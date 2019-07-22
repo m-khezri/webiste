543dc54d445552bdf984066a3ce86126c6cbfe8a
mohammad <khezri@outlook.com>
2019-07-22 15:18:05 -0500

    done setup

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello, I'm here... </h1>
+      <button className='btn btn-primary'> Hello </button>
+    </div>
+  );
+}
+
+export default App;
