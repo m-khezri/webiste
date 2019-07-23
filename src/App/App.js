a8a35e1c4b5d81f2ff9ae247a61040aed19c224b
mohammad <khezri@outlook.com>
2019-07-23 12:10:06 -0500

    About me added

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.scss';
+import AboutMe from '../Pages/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello, I'm here... </h1>
       <button className='btn btn-primary'> Hello </button>
+      <AboutMe />
     </div>
   );
 }
